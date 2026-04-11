7083a01416c95cfdb310e3b747c83ab99b646e47
hanzalahakbar <hanzalahakbarkps@gmail.com>
2026-04-11 11:11:56 +0500

    fixed issues

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,4 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
